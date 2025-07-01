0af54273c4a55428256b92ddd2329d161626afc4
emilia5 <emiliacabanillas2008@gmail.com>
2025-06-30 21:33:13 -0300

    cambios finales

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
 from .models import paises
-from .models import Publicacion
 
-admin.site.register(Publicacion)
 admin.site.register(paises)
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,11 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import paises
-from .models import Publicacion
-
-from django.contrib.auth.models import User
-# Create your views here.
-def lista_public(request):
-    publicaciones=Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('-fecha_publicacion')
-    usr_id=request.GET.get('usuario')
-    
-    return render(request, 'blog/lista_public.html', {
-        'publicaciones':publicaciones})
 
 def lista_paises(request):
-    PAISES=paises.objects.all()    
-    
-    return render(request, 'blog/lista_paises.html',
-    {'paises':PAISES})
+    posts = (
+        paises.objects
+        .filter(fecha_publicacion__lte=timezone.now()) 
+        .order_by('-fecha_publicacion')                
+    )
+    return render(request, 'blog/lista_paises.html', {'paises': posts})
